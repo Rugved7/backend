337f5a36fa46d97f352c9f96539bf465a9e6c3f6
Rugved7 <rugvedagasti7@gmail.com>
2024-06-27 16:58:18 +0530

    custom ApiError and Response logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./database/db.js"
+import { app } from "./app.js"
 
 dotenv.config({
   path: "./env",
 })
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5050, () => {
+      console.log(`Server Connection Eastablished`)
+    })
+  })
+  .catch((err) => {
+    console.log(`Server Connection Failed`, err)
+    throw err
+  })
