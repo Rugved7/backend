ddafb7ac248a59481563dfe52d2a59fe7750a598
Rugved7 <rugvedagasti7@gmail.com>
2024-06-29 13:52:02 +0530

    Cloudinary-multer setup done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,7 +58,7 @@ userSchema.pre("save", async function (next) {
   next()
 })
 
-// check if passwords match
+// matching passwords
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password)
 }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+const fileUpload = async (Filepath) => {
+  try {
+    if (!Filepath) return null
+    const response = await cloudinary.uploader.upload(Filepath, {
+      resource_type: "auto",
+    })
+    // File has been uploaded
+    console.log("File uploaded Successfully", response.url)
+    return response
+  } catch (error) {
+    fs.unlinkSync(Filepath)
+    return null
+  }
+}
+
+export { fileUpload }
